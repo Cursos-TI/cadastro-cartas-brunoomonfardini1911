f23104947a7bba44bf4ea23eaab97416fd1d2e6b
brunoomonfardini1911 <brunoomonfardini@gmail.com>
2026-03-07 02:32:03 +0000

    Concluí o desafio nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,70 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado [5];
+char codigo [5];
+char cidade [20];
+int populacao;
+float area;
+float pib;
+int pontos;
   // Área para entrada de dados
+printf("Insira os dados da carta 1 \n");
+printf("Estado: ");
+scanf("%s", estado);
+printf("Código: ");
+scanf("%s", codigo);
+printf("Cidade: ");
+scanf("%s", cidade);
+printf("População: ");
+scanf("%d", &populacao);
+printf("Área: ");
+scanf("%f", &area);
+printf("PIB: ");
+scanf("%f", &pib);
+printf("Número de pontos turísticos: ");
+scanf("%d", &pontos);
+
 
   // Área para exibição dos dados da cidade
+printf("------------------------------ \n");
+printf("Carta 1 \n");
+printf("Estado: %s \n", estado);
+printf("Código: %s \n", codigo);
+printf("Cidade: %s \n", cidade);
+printf("População: %d \n", populacao);
+printf("Área: %f \n", area);
+printf("PIB %f \n", pib);
+printf("Pontos turísticos: %d \n", pontos);
+printf("------------------------------ \n");
+
+//Informações carta 2
+
+printf("Insira os dados da carta 2 \n");
+printf("Estado: ");
+scanf("%s", estado);
+printf("Código: ");
+scanf("%s", codigo);
+printf("Cidade: ");
+scanf("%s", cidade);
+printf("População: ");
+scanf("%d", &populacao);
+printf("Área: ");
+scanf("%f", &area);
+printf("PIB: ");
+scanf("%f", &pib);
+printf("Número de pontos turísticos: ");
+scanf("%d", &pontos);
+
+printf("------------------------------ \n");
+printf("Carta 2 \n");
+printf("Estado: %s \n", estado);
+printf("Código: %s \n", codigo);
+printf("Cidade: %s \n", cidade);
+printf("População: %d \n", populacao);
+printf("Área: %f \n", area);
+printf("PIB %f \n", pib);
+printf("Pontos turísticos: %d \n", pontos);
 
 return 0;
 } 
